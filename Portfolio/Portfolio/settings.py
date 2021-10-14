8975055235213c9c7077e4268f84855eeb23df5d
Khalil <khalilr432@gmail.com>
2021-10-14 15:31:32 -0400

    config postgres

--- Portfolio/Portfolio/settings.py
+++ Portfolio/Portfolio/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': 'khalil',
+        'HOST': 'localhost',
+        'PORT' : '5432',
     }
 }
 
--- Portfolio/Portfolio/urls.py
+++ Portfolio/Portfolio/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+import job.views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', job.views.home, name="home")
     
 ]

b50c283f0b559e22b7d5fa4d68f8d62ad243e3d4
Khalil <khalilr432@gmail.com>
2021-10-20 17:27:07 -0400

    personalize bootstrap template

--- Portfolio/Portfolio/urls.py
+++ Portfolio/Portfolio/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import job.views
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', job.views.home, name="home")
+    path('', job.views.home, name="home")
     
-]
+] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
--- Portfolio/job/views.py
+++ Portfolio/job/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import job
 # Create your views here.
 def home(request):
-    return render(request, 'job/home.html')
+    jobs = job.objects
+    return render(request, 'job/home.html', {'jobs': jobs})

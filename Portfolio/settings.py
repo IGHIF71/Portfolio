cf026d787e789e3277d3f1b81ab0f8132e7e7ac5
Khalil <khalilr432@gmail.com>
2022-04-07 15:25:32 -0400

    Activating django heroku

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 import os
 from decouple import config
@@ -136,3 +136,6 @@ MEDIA_ROOT = BASE_DIR
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+django_heroku.settings(locals())
